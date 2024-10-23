f58204e85fef216cdf3461c9e8d032dfc20e36d6
Alexis Meier <Alexismeier1@gmail.com>
2024-10-23 10:10:02 -0300

    Fix problems and add foot section

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,17 @@ module.exports = {
         customGreenIcon: "#8e9e48",
         customOrangeIcon: "#f4c323"
       }
-    },
+    }
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.fade-image': {
+          '-webkit-mask-image': 'linear-gradient(to bottom, rgba(0, 0, 0, 1) 40%, rgba(0, 0, 0, 0) 100%)',
+          'mask-image': 'linear-gradient(to bottom, rgba(0, 0, 0, 1) 40%, rgba(0, 0, 0, 0) 100%)',
+        }
+      }
+      addUtilities(newUtilities)
+    }
+  ]
 }
-
